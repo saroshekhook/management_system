bece3eacc0e0e7b99c8ec0db42558ec6fc4bb934
saroprocedars <saro.procedars@gmail.com>
2023-11-01 18:01:16 +0400

    Added Authorization functionality. Validation functionality to be added

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,6 +2,7 @@ import { NextFunction, Response, Request } from "express";
 import jwt, { VerifyErrors } from "jsonwebtoken";
 import { getJWTSecret } from "../util/getJWTSecret";
 import { RequestWithUser } from "../types/types";
+import { USER_Type } from "../models/User";
 
 export async function isAuth(req: Request, res: Response, next: NextFunction) {
   const authHeader = req.headers["authorization"];
@@ -11,7 +12,26 @@ export async function isAuth(req: Request, res: Response, next: NextFunction) {
 
   try {
     const decodedToken = await jwt.verify(token, getJWTSecret());
-    (req as RequestWithUser).user = decodedToken;
+    (req as RequestWithUser).user = decodedToken; // TODO check correct ts
     next();
   } catch (err) {}
+}
+
+export function isAuthorized(...type: USER_Type[]) {
+  return function (req: Request, res: Response, next: NextFunction) {
+    const userPayload = (req as RequestWithUser)["user"]; // TODO check correct ts
+    if (!userPayload) {
+      return res.sendStatus(401);
+    }
+
+    if (typeof userPayload === "string") {
+      return res.sendStatus(403);
+    }
+
+    if (Array.isArray(type) && !type.includes(userPayload.type)) {
+      return res.sendStatus(403);
+    }
+
+    next();
+  };
 }
--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -5,11 +5,13 @@ import {
   updateTask,
   deleteTask,
 } from "../controllers/taskController";
+import { isAuthorized } from "../middlewares/auth";
+import { USER_Type } from "../models/User";
 
 const router = express.Router();
 
 // Table routes
-router.get("/", getAllTasks);
+router.get("/", isAuthorized(USER_Type.admin, USER_Type.head), getAllTasks);
 router.post("/", createTask);
 router.put("/:id", updateTask);
 router.delete("/:id", deleteTask);
